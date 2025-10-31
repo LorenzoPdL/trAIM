68e94206436808e4ca50ca8f0808972172035787
lprnzz <lorenzopdleite@gmail.com>
2025-10-31 17:29:38 -0300

    controller created

---
+++ src/main/java/net/lprnzz/traim/Controller.java
@@ -0,0 +1,18 @@
+package net.lprnzz.traim;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "boas vindas nessa rota";
+    }
+
+
+}
