b2da1f74fecf92c853bc72bbd4b04efced31cb26
lprnzz <lorenzopdleite@gmail.com>
2025-10-31 14:41:54 -0300

    first commit

---
+++ src/main/java/net/lprnzz/traim/TraimApplication.java
@@ -0,0 +1,13 @@
+package net.lprnzz.traim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/lprnzz/traim/TraimApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lprnzz.traim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
